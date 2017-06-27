6ea44309dcdc1e9f1c0479e0d958528318cab403
Osama Nehme <onehdev@gmail.com>
2017-06-27 18:03:39 +0200

    Change buttons caption + Updated Welcome messages

--- app/js/master.js
+++ app/js/master.js
@@ -1,1 +1,13 @@
 // GLOBAL JS
+function changeCaption(data){
+  var $container = $('#hu-webchat-messages .hu-messenger-message:last-child');
+  if ($container.length > 0) {
+    var $buttons = $container.find('.hu-btn-group-vertical .hu-btn span');
+    $buttons.each(function(index, btn){
+      var __key = $(btn).text().toLowerCase();
+      if ( data[__key] ) {
+        $(btn).text( data[__key] );
+      }
+    })
+  }
+}
