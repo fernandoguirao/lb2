d3099fc67f26a75f9a5998e40649781f01eab07b
Osama Nehme <onehdev@gmail.com>
2017-07-10 11:19:03 +0200

    Selectize plugin fix

--- dynamic/landbot-1.0.0.js
+++ dynamic/landbot-1.0.0.js
@@ -302,7 +302,7 @@ Landbot.prototype.generateDocument = function generateDocument() {
   }
 
   // selectize.js
-  this.generateTag('meta', head, {
+  this.generateTag('script', head, {
     type: 'text/javascript',
     src: 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js',
     charset: 'UTF-8'
