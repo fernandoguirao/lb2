0822ba756f08d133b91b5fe058a34149b9b7660f
Osama Nehme <onehdev@gmail.com>
2017-07-10 11:28:29 +0200

    Selectize plugin fix (final)

--- dynamic/landbot-1.0.0.js
+++ dynamic/landbot-1.0.0.js
@@ -299,20 +299,21 @@ Landbot.prototype.generateDocument = function generateDocument() {
       integrity: 'sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=',
       crossOrigin: 'anonymous'
     });
-  }
 
-  // selectize.js
-  this.generateTag('script', head, {
-    type: 'text/javascript',
-    src: 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js',
-    charset: 'UTF-8'
-  });
-  // selectize.css
-  this.generateTag('link', head, {
-    rel: 'stylesheet',
-    href: 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.min.css',
-    type: 'text/css'
-  });
+    // selectize.js
+    this.generateTag('script', head, {
+      type: 'text/javascript',
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js',
+      charset: 'UTF-8'
+    });
+
+    // selectize.css
+    this.generateTag('link', head, {
+      rel: 'stylesheet',
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/css/selectize.min.css',
+      type: 'text/css'
+    });
+  }
 
   // master.js
   this.generateTag('script', head, {
